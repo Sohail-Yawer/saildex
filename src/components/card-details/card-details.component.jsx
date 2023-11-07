db390ddd2a48d91a9c276346603da5b75666b29a
Sohail-Yawer <sohailyawer9@gmail.com>
2023-11-06 18:38:43 -0800

    getting poke details from API

--- src/components/card-details/card-details.component.jsx
+++ src/components/card-details/card-details.component.jsx
@@ -1,28 +1,67 @@
-import { Component } from "react";
-import {Link, withRouter} from 'react-router-dom';
+import React, { Component } from 'react';
+import {  Link,withRouter } from 'react-router-dom';
 import './card-details.style.css';
 
 class CardDetails extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pokemonDetails: null,
+    };
+  }
 
   handleBack = () => {
-    this.props.history.push('/'); // Navigate to the CardList component
+    this.props.history.push('/');
     window.open(`/`,"_self");
   };
 
-    render() {
-      const { match } = this.props;
-      const pokemonName = match.params.name; // Extract the Pokemon name from the route
-  
-      // Fetch and display Pokemon details based on the 'pokemonName'
-  
-      return (
-        <div style={{ position: 'relative' }}>
-        <Link to="/" className="back-button" onClick={this.handleBack}>&lt; Back</Link>
-        <h2>Pokemon Details for: {pokemonName}</h2>
+  componentDidMount() {
+    const { match } = this.props;
+    const pokemonName = match.params.name;
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ pokemonDetails: data });
+      })
+      .catch((error) => {
+        console.error('Error fetching Pokemon details:', error);
+      });
+  }
+
+  render() {
+    const { pokemonDetails } = this.state;
+
+    if (!pokemonDetails) {
+      return <div>Loading...</div>;
+    }
+
+    return (
+      <div >
+        <div>
+          <Link className="back-button" onClick={this.handleBack} >&lt; Back</Link>
+        </div>
+
+        <div>
+          <h2>Pokemon Details for: {pokemonDetails.name}</h2>
+          <h3>Abilities:</h3>
+          <ol>
+            {pokemonDetails.abilities.map((ability, index) => (
+              <li key={index}>{ability.ability.name}</li>
+            ))}
+          </ol>
+          <h3>Types:</h3>
+          <ol>
+            {pokemonDetails.types.map((type, index) => (
+              <li key={index}>{type.type.name}</li>
+            ))}
+          </ol>
+        </div>
         {/* Display other Pokemon details here */}
       </div>
-      );
-    }
+    );
   }
-  
-  export default withRouter(CardDetails);
+}
+
+export default withRouter(CardDetails);
